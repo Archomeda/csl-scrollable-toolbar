38e79a059cdfc426187552c71ab65b4921a2e60b
Archomeda <archomeda@hotmail.com>
2015-06-01 00:16:39 +0200

    Update cache and add Non-Interactive to commands

--- appveyor/Build.ps1
+++ appveyor/Build.ps1
@@ -1,6 +1,6 @@
 # Restore our NuGet packages
-nuget sources Add -Name CitiesSkylines -Source $env:NUGET_CSL_URL -UserName $env:NUGET_CSL_USERNAME -Password $env:NUGET_CSL_PASSWORD -Verbosity quiet
-nuget restore .\appveyor\packages.config -SolutionDirectory .\
+nuget sources Add -Name CitiesSkylines -Source $env:NUGET_CSL_URL -UserName $env:NUGET_CSL_USERNAME -Password $env:NUGET_CSL_PASSWORD -Verbosity quiet -Non-Interactive
+nuget restore .\appveyor\packages.config -SolutionDirectory .\ -Non-Interactive
 
 [xml]$packages = Get-Content .\appveyor\packages.config
 $referencePath = ($packages.packages.package | % {"..\packages\$($_.id).$($_.version)\lib\$($_.targetFramework)\"}) -Join ";"
