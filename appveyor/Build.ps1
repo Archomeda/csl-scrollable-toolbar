cdcfc720ad077fb5c4b9fbac8e6aaea3d2a8330e
Archomeda <archomeda@hotmail.com>
2015-06-01 00:22:34 +0200

    Fix wrong argument

--- appveyor/Build.ps1
+++ appveyor/Build.ps1
@@ -1,6 +1,6 @@
 # Restore our NuGet packages
-nuget sources Add -Name CitiesSkylines -Source $env:NUGET_CSL_URL -UserName $env:NUGET_CSL_USERNAME -Password $env:NUGET_CSL_PASSWORD -Verbosity quiet -Non-Interactive
-nuget restore .\appveyor\packages.config -SolutionDirectory .\ -Non-Interactive
+nuget sources Add -Name CitiesSkylines -Source $env:NUGET_CSL_URL -UserName $env:NUGET_CSL_USERNAME -Password $env:NUGET_CSL_PASSWORD -Verbosity quiet -NonInteractive
+nuget restore .\appveyor\packages.config -SolutionDirectory .\ -NonInteractive
 
 [xml]$packages = Get-Content .\appveyor\packages.config
 $referencePath = ($packages.packages.package | % {"..\packages\$($_.id).$($_.version)\lib\$($_.targetFramework)\"}) -Join ";"
