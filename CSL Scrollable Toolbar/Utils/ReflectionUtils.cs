272e569488a2500503db1226ce456388417b732d
Archomeda <archomeda@hotmail.com>
2015-06-02 20:03:25 +0200

    Change method visibility to public

--- CSL Scrollable Toolbar/Utils/ReflectionUtils.cs
+++ CSL Scrollable Toolbar/Utils/ReflectionUtils.cs
@@ -8,35 +8,35 @@ namespace ScrollableToolbar.Utils
 {
     internal static class ReflectionUtils
     {
-        internal static T GetPrivateField<T>(object obj, string name)
+        public static T GetPrivateField<T>(object obj, string name)
         {
             return (T)obj.GetType().GetField(name, BindingFlags.Instance | BindingFlags.NonPublic).GetValue(obj);
         }
 
-        internal static void SetPrivateField<T>(object obj, string name, T value)
+        public static void SetPrivateField<T>(object obj, string name, T value)
         {
             obj.GetType().GetField(name, BindingFlags.Instance | BindingFlags.NonPublic).SetValue(obj, value);
         }
 
-        internal static void InvokePrivateMethod(object obj, string name, params object[] args)
+        public static void InvokePrivateMethod(object obj, string name, params object[] args)
         {
             MethodInfo method = obj.GetType().GetMethod(name, BindingFlags.Instance | BindingFlags.NonPublic);
             method.Invoke(obj, args);
         }
 
-        internal static T InvokePrivateMethod<T>(object obj, string name, params object[] args)
+        public static T InvokePrivateMethod<T>(object obj, string name, params object[] args)
         {
             MethodInfo method = obj.GetType().GetMethod(name, BindingFlags.Instance | BindingFlags.NonPublic);
             return (T)method.Invoke(obj, args);
         }
 
-        internal static void InvokePrivateStaticMethod(Type type, string name, params object[] args)
+        public static void InvokePrivateStaticMethod(Type type, string name, params object[] args)
         {
             MethodInfo method = type.GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic);
             method.Invoke(null, args);
         }
 
-        internal static T InvokePrivateStaticMethod<T>(Type type, string name, params object[] args)
+        public static T InvokePrivateStaticMethod<T>(Type type, string name, params object[] args)
         {
             MethodInfo method = type.GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic);
             return (T)method.Invoke(null, args);
